f19b9f5cc4b1d13a87570d81d193c064ca8e04f6
mfsun00 <365devops@gmail.com>
2021-04-23 17:39:05 +0900

    init

---
+++ src/main/kotlin/com/example/ncif/NcifApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ncif
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NcifApplication
+
+fun main(args: Array<String>) {
+    runApplication<NcifApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/ncif/NcifApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.ncif
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NcifApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
